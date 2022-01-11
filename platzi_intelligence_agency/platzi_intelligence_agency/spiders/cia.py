9f5b15076beae94449c0d0e8b623c19f222370bb
Fabian Ardila <fabianra97@gmail.com>
2022-01-10 21:20:00 -0500

    :sparkles: Extract data

--- platzi_intelligence_agency/platzi_intelligence_agency/spiders/cia.py
+++ platzi_intelligence_agency/platzi_intelligence_agency/spiders/cia.py
@@ -2,6 +2,9 @@ import scrapy
 
 # //a[starts-with(@href,"collection") and (parent::h3|parent::h2)]/@href
 
+# title: //h1[@class="documentFirstHeading"]/text()
+# paragraph: //div[@class="field-item even"]/p[not(@class) and not(child::strong)]/text()
+
 class SpiderCia(scrapy.Spider):
     name = 'cia'
     start_urls = [
@@ -17,7 +20,15 @@ class SpiderCia(scrapy.Spider):
     def parse(self, response):
         links_declassified = response.xpath('//a[starts-with(@href,"collection") and (parent::h3|parent::h2)]/@href').getall()
         for link in links_declassified:
-            yield response.follow(link, callback=self.parse_link)
+            yield response.follow(link, callback=self.parse_link, cb_kwargs={'url':response.urljoin(link)})
 
-    def parse_link(self, response):
-        pass
+    def parse_link(self, response, **kwargs):
+        link = kwargs['url']
+        title = response.xpath('//h1[@class="documentFirstHeading"]/text()').get()
+        paragraph = response.xpath('//div[@class="field-item even"]/p[not(@class) and not(child::strong)]/text()').get()
+
+        yield {
+            'url': link,
+            'title': title,
+            'body': paragraph
+        }
